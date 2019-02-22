c164aebb562956a72fbe6a6f0e8859c8aa6087dc
danshapero <shapero.daniel@gmail.com>
2019-02-22 11:12:40 -0800

    Fix to updated rasterio API in modis mosaics

--- modis_mosaics/partition.py
+++ modis_mosaics/partition.py
@@ -7,23 +7,22 @@ def main(regions_file, big_image):
     with open(regions_file, 'r') as geojson_file:
         regions = geojson.loads(geojson_file.read())
 
-    with rasterio.drivers():
-        with rasterio.open(big_image, 'r') as source:
-            for region in regions['features']:
-                name = region['properties']['name'].lower()
-                box = region['geometry']['coordinates']
-                window = source.window(*box[0], *box[1])
+    with rasterio.open(big_image, 'r') as source:
+        for region in regions['features']:
+            name = region['properties']['name'].lower()
+            box = region['geometry']['coordinates']
+            window = source.window(*box[0], *box[1])
 
-                image = source.read(window=window)
+            image = source.read(window=window)
 
-                profile = source.profile
-                transform = source.window_transform(window)
-                profile['transform'] = transform
-                profile['height'] = image.shape[1]
-                profile['width'] = image.shape[2]
+            profile = source.profile
+            transform = source.window_transform(window)
+            profile['transform'] = transform
+            profile['height'] = image.shape[1]
+            profile['width'] = image.shape[2]
 
-                with rasterio.open(name + ".tif", 'w', **profile) as dest:
-                    dest.write(image)
+            with rasterio.open(name + ".tif", 'w', **profile) as dest:
+                dest.write(image)
 
 
 if __name__ == "__main__":
